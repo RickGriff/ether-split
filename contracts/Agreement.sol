6a25347ee9aaa96db78f8b61721a6ef9c7fed3a0
RickGriff <32799176+RickGriff@users.noreply.github.com>
2018-11-09 16:23:03 +0000

    Implement confirmAll function

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -7,7 +7,29 @@ contract Agreement {
   address public invited_friend;
   address public user_2;
 
- //  ***** constructor and user registration functions *****
+  int public balance;  // the net balance of who owes who.  Positive if user_2 owes more, negative if user_1 owes more.
+
+  // initialize uint lengths of transaction arrays. Useful because public variables have automatic getters, so we
+  // can easily grab array lengths for testing and healthchecks.
+  uint public pendingTxs1Length;
+  uint public pendingTxs2Length;
+  uint public confirmedTxsLength;
+
+  Tx[] public pendingTransactions_1;  // pending transactions to be confirmed by user 1
+  Tx[] public pendingTransactions_2;  // pending transactions to be confirmed by user 2
+  Tx[] public confirmedTransactions;  // All Transactions approved by users
+
+  // the basic transaction object
+  struct Tx {
+    uint amount;
+    bool split;
+    address creator;
+    address confirmer;
+    address debtor;
+    string description;
+  }
+
+  //  ***** constructor and user registration functions *****
 
   constructor() public {
     user_1 = msg.sender;
@@ -15,31 +37,139 @@ contract Agreement {
 
   function inviteFriend(address _friend) onlyUser1 onlyUser2NotRegistered public {
     invited_friend = _friend;
+  }
+
+  function registerUser2() onlyInvitedFriend onlyUser2NotRegistered public {
+    user_2 = msg.sender;
+  }
+
+  // ****** Functions for creating and confirming transactions
+
+  function createPending(uint _amount, bool _split, address _debtor, string _description) onlyUser public {
+    require( _debtor == user_1 || _debtor == user_2, 'debtor must be a registered user' );
+    require( bytes(_description).length < 35, 'Description too long' );   // string length isn't always *exactly* bytes length - but this nevertheless enforces a short description.
+
+    // create new pending tx
+    Tx memory newPendingTx;
+
+    // set the other user as confirmer
+    newPendingTx.confirmer = getOtherUser(msg.sender);
+
+    // set remaining attributes
+    newPendingTx.amount = _amount;
+    newPendingTx.split = _split;
+    newPendingTx.creator = msg.sender;
+    newPendingTx.debtor = _debtor;
+    newPendingTx.amount = _amount;
+    newPendingTx.description = _description;
+
+    // append new tx to the confirmer's pending tx array, and updated it's length
+    if (newPendingTx.confirmer == user_1) {
+      pendingTransactions_1.push(newPendingTx);
+      pendingTxs1Length = getPendingTxsLength1();
+
+    } else if (newPendingTx.confirmer == user_2) {
+      pendingTransactions_2.push(newPendingTx);
+      pendingTxs2Length = getPendingTxsLength2();
+    }
+  }
+
+  function confirmAll() onlyUser public {
+        Tx[] storage allPendingTx = getPendingTx(msg.sender);
+
+        Tx[] memory memAllPendingTx = allPendingTx;  // copy pending txs to memory
+
+        allPendingTx.length = 0; // delete all elements in pending tx array
+        pendingTxs1Length = getPendingTxsLength1();  //update stored lengths of pending tx arrays
+        pendingTxs2Length = getPendingTxsLength2();
+        int balanceChange  = 0;
+
+        for (uint i = 0; i < memAllPendingTx.length; i++) {
+            confirmedTransactions.push(memAllPendingTx[i]);  // add Tx to confirmed array
+            balanceChange = balanceChange + changeInBalance(memAllPendingTx[i]); // add the transaction amount to the balance change */
+        }
+        confirmedTxsLength = getConfirmedTxsLength();
+        balance = balance + balanceChange;  // update the balance in storage
     }
 
-    function registerUser2() onlyInvitedFriend onlyUser2NotRegistered public {
-         user_2 = msg.sender;
-     }
+  // ***** Helper and getter functions *****
 
+  function changeInBalance(Tx _purchase) private view returns (int _change) {
+        // returns the change to a balance caused by a purchase
+        int change = 0;
+        if (_purchase.split == true) {
+        return change;  // no overall change to balance when an expense is split
+        } else if (_purchase.debtor == user_1) {
+            change = -int(_purchase.amount);
+            return change;
+        } else if (_purchase.debtor == user_2) {
+            change = int(_purchase.amount);
+            return change;
+        }
+    }
 
-// ******** Modifiers *********
-modifier onlyUser {
-       require(msg.sender == user_1 || msg.sender == user_2, 'Must be a registered user');
-       _;
-   }
+  function getPendingTx( address _user) private view returns (Tx[] storage){
+        // return the user's pending transactions list
+        Tx[] storage allPendingTx;
+        if (_user == user_1) {
+            allPendingTx = pendingTransactions_1;
+        } else if (_user == user_2) {
+            allPendingTx = pendingTransactions_2;
+        }
+        return allPendingTx;
+    }
+
+  function getOtherUser(address _user) private view returns (address) {
+    require(_user == user_1 || _user == user_2, 'user must be registered');
+    if (_user == user_1) {
+      return user_2;
+    } else if (_user == user_2) {
+      return user_1;
+    }
+  }
+
+  function getPendingTxsLength1() internal view returns(uint) {
+    return pendingTransactions_1.length;
+  }
+
+  function getPendingTxsLength2() internal view returns(uint) {
+    return pendingTransactions_2.length;
+  }
+
+  function getConfirmedTxsLength() internal view returns(uint) {
+    return confirmedTransactions.length;
+  }
+
+  /* function pendingTransactions2Length() onlyUser external view returns (uint) {
+    return pendingTransactions_2.length;
+  } */
+
+  /* function pendingTransactions1Length() onlyUser external view returns (uint) {
+    return pendingTransactions_1.length;
+  }
+
+   function confirmedTransactionsLength() onlyUser external view returns (uint) {
+    return confirmedTransactions.length;
+  } */
+
+  // ******** Modifiers *********
+  modifier onlyUser {
+    require(msg.sender == user_1 || msg.sender == user_2, 'Must be a registered user');
+  _;
+  }
 
   modifier onlyUser1 {
     require(msg.sender == user_1, 'Must be registered user 1');
-    _;
+  _;
   }
 
   modifier onlyInvitedFriend {
-        require(msg.sender == invited_friend, 'Must be the invited friend');
-        _;
-    }
+    require(msg.sender == invited_friend, 'Must be the invited friend');
+  _;
+  }
 
   modifier onlyUser2NotRegistered {
     require (user_2 == address(0), 'User 2 already registered');
-     _;
+  _;
   }
 }
--- test/AgreementTest.js
+++ test/AgreementTest.js
@@ -51,4 +51,165 @@ describe('Deployment and user registration', function() {
       }
     });
   });
+
+  describe('Create pending transactions', function() {
+    let agreement;
+
+  beforeEach(async () => {
+    agreement = await Agreement.new();
+    await agreement.inviteFriend(secondAccount);
+    await agreement.registerUser2({from: secondAccount});
+  });
+
+    it("creates a pending transaction as user_1", async () => {
+      await agreement.createPending(
+        amount=50,
+        split=false,
+        debtor=secondAccount,
+        description='I bought him sushi',
+      );
+
+      assert.equal(await agreement.pendingTxs1Length.call(), 0);
+      assert.equal(await agreement.pendingTxs2Length.call(), 1);  // tx is added to user_2's pending tx list
+    });
+
+    it("creates a pending transaction as user_2", async () => {
+      await agreement.createPending(
+        amount=1000,
+        split=false,
+        debtor=firstAccount,
+        description='I bought her a dog',
+        {from: secondAccount}  // user_2 creates tx
+      );
+
+      assert.equal(await agreement.pendingTxs1Length.call(), 1); // tx is added to user_1's pending tx list
+      assert.equal(await agreement.pendingTxs2Length.call(), 0);
+    });
+  });
+
+  describe("Confirm all pending transactions", function() {
+    let agreement;
+    // TODO Implement more concise instantiation of registered users / pending txs for use in tests
+    beforeEach(async () => {
+      agreement = await Agreement.new();
+      await agreement.inviteFriend(secondAccount);
+      await agreement.registerUser2({from: secondAccount});
+      // create 2 pending transactions as user_1
+      await agreement.createPending(
+        amount=30,
+        split=false,
+        debtor=secondAccount,
+        description='I bought her sushi',
+      );
+      await agreement.createPending(
+        amount=50,
+        split=false,
+        debtor=firstAccount,
+        description='She bought me Nandos',
+      );
+      // create 2 pending transactions as user_2
+      await agreement.createPending(
+        amount=500,
+        split=false,
+        debtor=firstAccount,
+        description='I bought him a bike',
+        {from: secondAccount}  // user_2 creates tx
+      );
+      await agreement.createPending(
+        amount=1,
+        split=false,
+        debtor=firstAccount,
+        description='I bought him a newspaper',
+        {from: secondAccount}
+      );
+      });
+
+      it ("has pending transactions for each user", async () => {
+        assert.equal(await agreement.pendingTxs1Length.call().valueOf(), 2);
+        assert.equal(await agreement.pendingTxs2Length.call().valueOf(), 2);
+      });
+
+
+      context("As user_1:", function() {
+        it("deletes all user_1's pending transactions", async () => {
+          await agreement.confirmAll();
+          let pending_tx_length = await agreement.pendingTxs1Length.call();
+          assert.equal(pending_tx_length.toNumber(), 0);
+        });
+
+        it("adds all user_1's pending tx to confirmedTransactions", async () => {
+
+          let conf_txs_length_before = await agreement.confirmedTxsLength.call()
+          assert.equal(conf_txs_length_before.toNumber(), 0 );
+          // first_tx = await agreement.pendingTransactions_1.call(0);
+          // second_tx = await agreement.pendingTransactions_1.call(1);
+
+          await agreement.confirmAll();
+
+          let conf_txs_length_after = await agreement.confirmedTxsLength.call()
+          assert.equal(conf_txs_length_after.toNumber(), 2);
+          // assert.equal(first, await agreement.confirmedTransactions.call(0));
+          // assert.equal(second, await agreement.confirmedTransactions.call(1));
+        });
+
+        it("doesn't affect user_2's pending tx", async () => {
+          let p2_length_before = await agreement.pendingTxs2Length.call();
+          assert.equal(p2_length_before.toNumber(), 2);
+
+          await agreement.confirmAll();
+
+          let p2_length_after = await agreement.pendingTxs2Length.call();
+          assert.equal(p2_length_after.toNumber(), 2);
+        });
+
+        it("updates balance in storage", async () => {
+          let bal_before = await agreement.balance.call();
+
+          await agreement.confirmAll();
+
+          let bal_after = await agreement.balance.call();
+          assert.equal(bal_after.toNumber(), bal_before.toNumber() - 501, ); // user_2 bought two things for user_1, of value 501.
+        });
+      });
+
+      context("As user_2:", function() {
+        it("deletes all user_2's pending transactions", async () => {
+          await agreement.confirmAll({from: secondAccount});
+          let pending_tx_length = await agreement.pendingTxs2Length.call();
+          assert.equal(pending_tx_length.toNumber(), 0);
+        });
+
+        it("adds all user_2's pending tx to confirmedTransactions", async () => {
+          let conf_txs_length_before = await agreement.confirmedTxsLength.call()
+          assert.equal(conf_txs_length_before.toNumber(), 0 );
+          // first_tx = await agreement.pendingTransactions_1.call(0);
+          // second_tx = await agreement.pendingTransactions_1.call(1);
+          await agreement.confirmAll({from: secondAccount});
+
+          let conf_txs_length_after = await agreement.confirmedTxsLength.call()
+          assert.equal(conf_txs_length_after.toNumber(), 2 );
+          // assert.equal(first, await agreement.confirmedTransactions.call(0));
+          // assert.equal(second, await agreement.confirmedTransactions.call(1));
+        });
+
+        it("doesn't affect user_1's pending tx", async () => {
+          let p1_length_before = await agreement.pendingTxs1Length.call();
+          assert.equal(p1_length_before.toNumber(), 2);
+
+          await agreement.confirmAll({from: secondAccount});
+
+          let p1_length_after = await agreement.pendingTxs1Length.call();
+          assert.equal(p1_length_after.toNumber(), 2);
+        });
+
+        it("updates balance in storage", async () => {
+          let bal_before = await agreement.balance.call();
+
+          await agreement.confirmAll({from: secondAccount});
+
+          let bal_after = await agreement.balance.call();
+          assert.equal(bal_after.toNumber(), bal_before.toNumber() - 20, ); // user_1 owed 50, user_2 owed 30.
+        });
+      });
+  });
 });
